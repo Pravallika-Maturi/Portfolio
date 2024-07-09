384ed389f1ae3217ba3efe7d49996909440964a3
Pravallika.Maturi <109272674+Pravallika-Maturi@users.noreply.github.com>
2024-07-09 21:39:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text" ,{
+    strings:["Frontend Developer","Graphic Designer","Programmer","UI/UX Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
